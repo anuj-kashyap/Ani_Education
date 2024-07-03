fe86397b21bca547244411cce047b5369b5168d4
anuj-kashyap <kashyapanuj713@gmail.com>
2024-07-03 18:12:53 +0530

    HeroSection

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import girl from '../assets/girl 1.png'; // Ensure you have the correct path to your image
+import girl from '../assets/girl 1.png';
+import flare from '../assets/flare.png'
 
 const Hero = () => {
   return (
-    <section className="relative bg-blue-950 text-white px-8 rounded-bl-[150px]">
+    <section className="relative bg-blue-950 text-white px-8 rounded-bl-[150px] overflow-hidden">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <div className="md:w-1/2">
           <h1 className="text-8xl font-bold mb-4">Learning</h1>
           <h1 className="text-6xl font-bold mb-4"> Without Limits</h1>
-          
           <p className="text-lg mb-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.
           </p>
           <div className="flex items-center space-x-4">
@@ -23,8 +23,17 @@ const Hero = () => {
             </button>
           </div>
         </div>
-        <div className="md:w-1/4 md:mt-10">
-          <img src={girl} alt="Hero" className="rounded-lg" />
+        <div className="md:w-1/4 md:mt-10 relative">
+          <img 
+            src={flare} 
+            alt="" 
+            className='absolute w-[600px] h-[550px] top-[-130px] right-[-1px] z-0'
+          />
+          <img 
+            src={girl} 
+            alt="Hero" 
+            className="rounded-lg z-10 relative right-[-20px]"
+          />
         </div>
       </div>
     </section>
